a7a1592883fd282f2d3372833ef76fec8bfd9562
caiohamamura <caiohamamura@gmail.com>
2019-07-24 22:58:47 -0300

    Fix H5File help link

--- R/readLevel1b.R
+++ R/readLevel1b.R
@@ -7,7 +7,7 @@
 #'@param level1bpath file path pointing to LVIS level1b data (H5 format)
 #'@return H5File; S4 object of class H5File;
 #'@author Carlos Alberto Silva. This function calls \emph{h5file} function from h5 package (Author: Mario Annau)
-#'@seealso \code{\link[h5]{h5file}} in the \emph{h5} package.
+#'@seealso \code{\link[h5]{H5File}} in the \emph{h5} package.
 #'@examples
 #'
 #'# LVIS level1b file path
